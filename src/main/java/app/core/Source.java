8ed92c78c4210f0263cdb15cdee96358629507a3
Yanuar Rizki Sanjaya <yanuarrizki165@gmail.com>
2022-06-07 13:16:39 +0700

    new Ko-Ma with maven

---
+++ src/main/java/app/core/Source.java
@@ -0,0 +1,8 @@
+package app.core;
+
+public class Source {
+    public static final String driverM =    "com.mysql.cj.jdbc.Driver",
+                               db_url  =    "jdbc:mysql://localhost/kokai_mae_app_test",
+                               user    =    "yanu",
+                               pass    =    "yanusukakopi";
+}
